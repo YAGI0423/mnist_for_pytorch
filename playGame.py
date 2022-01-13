aea3b9bd8824f508ca2661216330766d7399148c
YAGI0423 <9945735@naver.com>
2022-01-13 22:54:57 +0900

    update `user.py, playGame.py`

--- playGame.py
+++ playGame.py
@@ -1,31 +1,15 @@
 import gameBoard
 import model
+import user
 
 import numpy as np
 
 
-def check_input(message):
-    #check user input value
-    while True:
-        input_data = input(f"{message}: ")
-        if (input_data) == "": continue  #빈 값
-        try:
-            input_data = int(input_data)
-            return input_data
-        except:
-            continue
-
-
 player_info = {
-    'playerA': True,
+    'playerA': user.User(9),
     'playerB': model.AlphaO(9)
 }
-# if type(player_info['playerB']) == bool():
-print(type(player_info['playerB']) == 'class')
-exit()
-
 playerA_color = bool(np.random.randint(2))   #True: Black, Flase: White
-
 
 board = gameBoard.GameBoard(9)
 
@@ -33,37 +17,18 @@ while game_done := not board.check_game_over():
     print("=" * 100)
     print(board.get_square_board())
 
+    #Put Stone=======================
     now_turn = board.now_turn()
     print("Black" if now_turn else "White")
 
     now_player_key = "playerA" if playerA_color == now_turn else "playerB"
-
-    # if player_info[now_player_key]:
-
-    # if black_player == now_turn:
-    #     if player1:
-    #         while True:
-    #             input_x, input_y = check_input("x"), check_input("y")
-    #             success = board.put_stone(input_x, input_y)
-    #             if success: break;
-    #     else:
-    #         loc = model.act(board.get_list_board)
-    #         success = board.put_stone(loc)
-    # else:
-    #     if player2:
-    #         while True:
-    #             input_x, input_y = check_input("x"), check_input("y")
-    #             success = board.put_stone(input_x, input_y)
-    #             if success: break;
-    #     else:
-    #         loc = model.act(board.get_list_board())
-    #         success = board.put_stone(*loc)
-
-
+    stone_location = player_info[now_player_key].act(board.get_list_board())
+    success = board.put_stone(*stone_location)
+    #End=============================
 
     print("=" * 100, end="\n\n")
 
-if win_stone := board.now_turn():   #True: 백 승, False: 흑 승
+if now_turn:   #True: 백 승, False: 흑 승
     print("White Win")
 else:
     print("Black Win")
--- user.py
+++ user.py
@@ -1,3 +1,31 @@
 class User:
-    def __init__(self):
-        
+    def __init__(self, board_size):
+        self.board_size = board_size
+
+    def act(self, board):
+        def get_able_location(board):
+            #return able location
+            able_loc = set(
+                (x, y) \
+                for x in range(self.board_size) \
+                for y in range(self.board_size)
+            )
+            able_loc -= set(board)
+            return tuple(able_loc)
+
+        def check_input(message):
+            #check user input value
+            while True:
+                input_data = input(f"{message}: ")
+                if (input_data) == "": continue  #빈 값
+                try:
+                    input_data = int(input_data)
+                    return input_data
+                except:
+                    continue
+
+        able_loc = get_able_location(board)
+        while True:
+            input_x, input_y = check_input("x"), check_input("y")
+            if (input_x, input_y) in able_loc: break;
+        return (input_x, input_y)
