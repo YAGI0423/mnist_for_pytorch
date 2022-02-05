05ba209bcab48c13705fe77a363e24dddefb4155
YAGI0423 <9945735@naver.com>
2022-02-06 02:29:59 +0900

    update `model.py AlphaO`

--- model.py
+++ model.py
@@ -109,7 +109,7 @@ class AlphaO():
 
         #Select Branch
         #가지 선택 과제 수행 필요
-        for round in range(1):
+        for round in range(5):
             node = root
             branch_idx = select_branch(node)
 
