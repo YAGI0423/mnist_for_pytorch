261abc2ff74bd4356b41c86297e8d5b92d4dc99f
YAGI0423 <9945735@naver.com>
2022-05-30 17:31:41 +0900

    train model

--- GUI.py
+++ GUI.py
@@ -1,39 +1,47 @@
-import tkinter as tk
+from tkinter import *
 
 class GUI:
     def __init__(self, board_size, black_info, white_info):
+        self.board_size = board_size
         self.black_info, self.white_info = black_info, white_info
+
+        self.board_wh = 640
         
-        self.window = tk.Tk()
-        self.init_window(board_size)
+        self.root = Tk()
+        self.window = Canvas(self.root, width=700, height=800)
+        
+        self.init_window()
 
-        self.window.bind('<Motion>', self.callback)
-
-    def init_window(self, board_size):
-        self.window.title('board window')
-        self.window.geometry('700x800+100+100')
-        self.window.resizable(False, False)
-
-        self.window.create_rectangle(10, 10, 100, 100)
+        
 
 
+    def init_window(self):
+        self.root.title('board window')
+        self.root.geometry('700x800+100+100')
+        self.root.resizable(False, False)
+        self.window.pack()
+
+        self.window.create_rectangle(30, 130, 30+self.board_wh, 130+self.board_wh)
+
+
+        for idx in range(self.board_size):
+            step_size = self.board_wh // self.board_size
+            
+            self.window.create_line(30, 130+(), 30+self.board_wh, 130+self.board_wh)
+            break
 
     # def callback(self, event):
     #     print(f'{event.x}, {event.y}')
         
     def print(self):
-        self.window.mainloop()
-
-    # def located(self):
-    #     label = tk.Label(self.window, text='하이')
-    #     label.pack()
+        self.root.mainloop()
 
 
 if __name__ == '__main__':
     now_board = ((5, 5), (2, 2), (3, 3))
 
     
-    gui = GUI(board_size=10, black_info=2, white_info=0)
+    gui = GUI(board_size=3, black_info=2, white_info=0)
 
-    gui.located()
+    # gui.located()
     gui.print()
