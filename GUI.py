38c3835cb9bee76214f5bc03ac2589e364e0895a
YAGI0423 <9945735@naver.com>
2022-05-31 10:30:20 +0900

    train model

--- GUI.py
+++ GUI.py
@@ -3,53 +3,65 @@ import tkinter.font
 
 class GUI:
     def __init__(self, board_size, black_info, white_info):
+        def get_step_tuple():
+            step_size = self.board_wh // (self.board_size - 1)
+            left_pix = self.board_wh % (self.board_size - 1)
+
+            step_list = list()
+            temp = step_size
+            while temp < self.board_wh:
+                if left_pix > 0:
+                    temp += 1
+
+                step_list.append(temp)
+
+                temp += step_size
+                left_pix -= 1
+            return tuple(step_list)
+
         def init_window(root):
             root.title('board window')
             root.geometry(f"{self.wd['width']}x{self.wd['height']}+100+100")
             root.resizable(False, False)
 
-        def init_board(root):
+        def init_board(root, step_tuple):
             board = Canvas(root, width=self.wd['width'], height=self.wd['height'])
+
+            
             board.create_rectangle(
                 self.bd['x'], self.bd['y'],
                 self.bd['x']+self.board_wh, self.bd['y']+self.board_wh
             )
 
-            step_size = self.board_wh // (self.board_size - 1)
-            left_pix = self.board_wh % (self.board_size - 1)
-
-
             #draw row, col=============
-            for step in range(step_size, self.board_wh-1, step_size):
+            for step in step_tuple:
                 board.create_line(    #row
                     self.bd['x'], self.bd['y']+step,
                     self.bd['x']+self.board_wh, self.bd['y']+step
                 )
-                # board.create_line(    #col
-                #     self.bd['x']+step, self.bd['y'],
-                #     self.bd['x']+step, self.bd['y']+self.board_wh
-                # )
+                board.create_line(    #col
+                    self.bd['x']+step, self.bd['y'],
+                    self.bd['x']+step, self.bd['y']+self.board_wh
+                )
             #End=======================
 
             #draw cross dot============
-            for idx in range((self.board_size - 2) ** 2):   #board inner range
-                x = idx % (self.board_size - 2) + 1
-                y = idx // (self.board_size - 2) + 1
-                
-                board.create_oval(
-                    self.bd['x'] - 3 + (x * step_size), self.bd['y'] - 3 + (y * step_size),
-                    self.bd['x'] + 3 + (x * step_size), self.bd['y'] + 3 + (y * step_size),
-                    fill='gray'
-                )
+            for step_y in step_tuple:
+                for step_x in step_tuple:
+                    board.create_oval(
+                        self.bd['x'] - 3 + step_x, self.bd['y'] - 3 + step_y,
+                        self.bd['x'] + 3 + step_x, self.bd['y'] + 3 + step_y
+                    )
             #End=======================
+
 
             board.pack()
 
         def init_state(root):
             stone_ft = tkinter.font.Font(size=20)
 
-            black_stone_txt = Label(root, text='●', font=stone_ft)
-            white_stone_txt = Label(root, text='○', font=stone_ft)
+            black_stone_txt = Label(root, text='● User', font=stone_ft)
+            white_stone_txt = Label(root, text='AlphaO ○', font=stone_ft)
     
 
             black_stone_txt.place(x=self.bd['x'], y=10)
@@ -62,13 +74,24 @@ class GUI:
         self.wd = {'width': 700, 'height': 800} #wd: `window`의 줌말
 
         self.bd = {'x': 30}   #bd: `board`의 줌말, 시작 x, y 좌표
+
+
+        self.step_tuple = get_step_tuple()
+        self.stone_size = self.step_tuple[0] // 1.2
+
+        self.space = self.bd['x'] + (self.stone_size // 2)
+
         self.board_wh = self.wd['width'] - self.bd['x'] * 2 #board size
         self.bd['y'] = self.wd['height'] - self.board_wh - self.bd['x'] #get bd_y
+        print(self.stone_size)
+
+
+        print(self.step_tuple)
 
 
         self.root = Tk()
         init_window(self.root)
-        init_board(self.root)
+        init_board(self.root, self.step_tuple)
 
         init_state(self.root)
         
@@ -92,7 +115,7 @@ if __name__ == '__main__':
     now_board = ((5, 5), (2, 2), (3, 3))
 
     
-    gui = GUI(board_size=19, black_info=2, white_info=0)
+    gui = GUI(board_size=10, black_info=2, white_info=0)
 
     # gui.located()
     gui.print()
