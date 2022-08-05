b6c45c3a02dbcab287366ce78847feccb56c1783
YAGI0423 <9945735@naver.com>
2022-08-05 14:28:29 +0900

    clear repo

--- gameBoard.py
+++
@@ -1,9 +0,0 @@
-class GameBoard():
-    def __init__(self):
-        self.__board = []
-
-    def get_board(self):
-        return tuple(self.__board.copy())
-
-    def put_stone(self, x, y):
-        self.__board.append((x, y))
